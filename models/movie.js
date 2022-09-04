0899ea6eeba2e47f2219c1a95c852868d66eba10
Maxim <kejerino@yandex.ru>
2022-09-04 15:16:19 +0800

    fix: fixed schema for trailers

--- models/movie.js
+++ models/movie.js
@@ -30,7 +30,7 @@ const movieSchema = new mongoose.Schema({
       message: 'Неправильная ссылка',
     },
   },
-  trailer: {
+  trailerLink: {
     type: String,
     required: true,
     validate: {
