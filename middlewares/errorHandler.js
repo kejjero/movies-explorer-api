e32590816f63469e249ee3112abe9ed2ba8f74d7
Maxim <kejerino@yandex.ru>
2022-09-04 15:13:12 +0800

    fix: fixed status code

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, _req, res, next) => {
-  if (err.code) {
-    return res.status(err.code).send({ message: err.message || 'Ошибка по умолчанию' });
+  if (err.statusCode) {
+    return res.status(err.statusCode).send({ message: err.message || 'Ошибка по умолчанию' });
   }
   res.status(500).send({ message: 'На сервере произошла ошибка' });
   return next();
