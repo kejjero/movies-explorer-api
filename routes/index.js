7ddcff04f3eeb26d53b172c941894eedadc2ee27
Maxim <kejerino@yandex.ru>
2022-09-04 15:13:58 +0800

    fix: fixed routes movies & users

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
+const authRouter = require('./auth');
+const users = require('./users');
+const movies = require('./movies');
 const NotFoundError = require('../errors/NotFoundError');
+
+router.use(authRouter);
 
 router.use(auth);
 
-router.use(require('./users'));
-router.use(require('./movies'));
+router.use(users);
+router.use(movies);
 
 router.use('*', (_, __, next) => next(new NotFoundError('Запрашиваемая страница не найдена')));
 
